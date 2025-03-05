79de2ea6e210b605a795bb0b4a202076af2f0f3b
smanaman <sanvatsman@gmail.com>
2025-03-05 10:06:35 +0530

    usecontext

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
-
+import './About.css'
 function About() {
   
   return (
     <>
     <Navbar/>
-   
+   <h1 className='about'>About Page</h1>
     </>
   )
 }
